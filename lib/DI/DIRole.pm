79af9a4a976d31a8a238773b0503e11e65ae8b7d
Pavel Pozdniak <pozdpl@gmail.com>
2013-09-30 08:32:25 +1100

    master Added beans for MongoDB

--- lib/DI/DIRole.pm
+++ lib/DI/DIRole.pm
@@ -1,3 +1,34 @@
 package DI::DIRole;
+use Moose::Role;
 
+has mongo =>(
+    is => 'ro',
+    isa => 'MongoDB::MongoClient',
+    lifecycle => 'Singleton',
+);
+
+has mongo_database_name => (
+    is       => 'ro',
+    isa      => 'Str',
+    default => 'markers',
+    lifecycle => 'Singleton',
+);
+
+has test_database => (
+    is => 'ro',
+    #isa => 'Misc::MongoDatabase',
+    block => sub{
+    	my $s = shift;
+    	Misc::MongoDatabase->get_database(
+    	   $s->param('mongo'), 
+    	   $s->param('database_name')
+    	);
+    },
+    dependencies => {
+    	'mongo' => 'mongo',
+    	'database_name' => 'mongo_database_name'
+    },
+    lifecycle => 'Singleton',
+    
+);
 1;
