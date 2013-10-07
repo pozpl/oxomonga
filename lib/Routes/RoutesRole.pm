cbd02dc6454d023ef46e9e8cf9cce6b38cd40680
Pavel Pozdniak <pozdpl@gmail.com>
2013-10-08 08:28:29 +1100

    master Added route for a form show

--- lib/Routes/RoutesRole.pm
+++ lib/Routes/RoutesRole.pm
@@ -11,7 +11,8 @@ my $current_working_directory  = dirname(__FILE__);
 
 router as {
     route '/markers/near/radius'  => 'markers_rest_controller.find_near_markers';
-    
+    route '/markers/edit/show/form' => 'markers_edit_controller.show_form';
+
     mount '/static' => Plack::App::File->new(root => $current_working_directory . '/../../resources/static');
     #mount '/static' => "Plack::App::File", (
     #    root     => '../resources/static',
