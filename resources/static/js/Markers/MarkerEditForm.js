dd025a1750977e537bb0f96b770905e7731d565a
Pavel Pozdniak <pozdpl@gmail.com>
2013-10-10 08:09:40 +1100

    master Declared class for edit form

---
+++ resources/static/js/Markers/MarkerEditForm.js
@@ -0,0 +1,11 @@
+define( "dojo/_base/declare", "dojo/_base/array",     "dojo/dom-class",
+	"dojo/dom-attr", "dojo/io/script", "dojo/query",
+    function declare(declare, array, domClass, domAttr, script, query){
+        return declare('Markers.MarkerEditForm', {
+           loadFormContent : function (){
+               var form = query('')
+           }
+        });
+
+    }
+);
