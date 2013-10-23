63f3a1853d3f730dd356f2c2b0b2ac3825076917
Pavel Pozdniak <pozdpl@gmail.com>
2013-10-24 08:22:30 +1100

    master FIxed some nasty errors in marker rendering

--- lib/Markers/MarkersController.pm
+++ lib/Markers/MarkersController.pm
@@ -16,7 +16,28 @@ sub find_near_markers(){
     $radius = $radius || 0;
     
     my @markers = $self->markers_repository->find_near_markers($longitude, $latitude, $radius);
-    return encode_json(\@markers);
+    my @markers_hrefs = $self->_markers_to_struct(\@markers);
+    return encode_json(\@markers_hrefs);
+}
+
+sub _markers_to_struct(){
+    my ($self, $markers) = @_;
+
+    my @markers_hrefs_array = ();
+    foreach my $marker (@{$markers}){
+        my $marker_hash = {
+             'id' => $marker->id(),
+             'user' => $marker->user(),
+             'latitude' => $marker->latitude(),
+             'longitude' => $marker->longitude(),
+             'time_of_creation' => $marker->time_of_creation(),
+             'description' => $marker->description(),
+             'images' => $marker->images(),
+        };
+        push  @markers_hrefs_array,  $marker_hash;
+    }
+
+    return @markers_hrefs_array;
 }
 
 1;
