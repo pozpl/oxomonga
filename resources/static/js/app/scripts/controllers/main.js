4b712ce07b67e0ed8815f3e0f88f942443f8ece2
Pavel Pozdniak <pozdpl@gmail.com>
2013-11-15 07:57:30 +1100

    master Adding angular libraries into the project

---
+++ resources/static/js/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('testAngularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
