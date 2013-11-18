a9519740f09a76ba66fd5e0455d3d6216133535f
Pavel Pozdniak <pozdpl@gmail.com>
2013-11-19 07:52:07 +1100

    master Some new steps to add angular-yandex-maps into the project

--- resources/static/js/app/scripts/app.js
+++ resources/static/js/app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('testAngularApp', [])
+angular.module('testAngularApp', ['ngRoute', 'yaMap'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
