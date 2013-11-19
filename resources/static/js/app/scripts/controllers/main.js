9beea986538a0ac2d54dec35c12b5fce72916c42
Pavel Pozdniak <pozdpl@gmail.com>
2013-11-19 18:31:40 +1100

    master Experiments with yandex maps

--- resources/static/js/app/scripts/controllers/main.js
+++ resources/static/js/app/scripts/controllers/main.js
@@ -1,10 +1,23 @@
 'use strict';
 
 angular.module('testAngularApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+
+        $scope.geoObjects =
+        {
+            geometry: {
+                type: 'Circle',
+                coordinates: [37.60, 55.76],
+                radius: 10000
+            },
+            properties: {
+                balloonContent: "Радиус круга - 10 км",
+                hintContent: "Подвинь меня"
+            }
+        };
+    });
