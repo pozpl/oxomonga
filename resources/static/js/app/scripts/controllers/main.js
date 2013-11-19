d2145029a64538f633475d93e08519d54beadd2e
Pavel Pozdniak <pozdpl@gmail.com>
2013-11-20 10:00:29 +1100

    master Draw markers from the backend!!!

--- resources/static/js/app/scripts/controllers/main.js
+++ resources/static/js/app/scripts/controllers/main.js
@@ -1,33 +1,74 @@
 'use strict';
 
 angular.module('GeoHashingApp')
-    .controller('MainCtrl', function ($scope) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
+    .controller('MainCtrl', function ($scope, $http) {
+//        $scope.geoObjects=[
+//            {
+//                geometry: {
+//                    type: 'Point',
+//                    coordinates: [37.8,55.8]
+//                },
+//                properties: {
+//                    iconContent: 'Метка',
+//                    balloonContent: 'Меня можно перемещать'
+//                }
+//            },
+//            {
+//                geometry: {
+//                    type: 'Point',
+//                    coordinates: [37.6,55.8]
+//                },
+//                properties: {
+//                    iconContent: '1',
+//                    balloonContent: 'Балун',
+//                    hintContent: 'Стандартный значок метки'
+//                }
+//            },
+//            {
+//                geometry: {
+//                    type: 'Point',
+//                    coordinates: [37.56,55.76]
+//                },
+//                properties: {
+//                    hintContent: 'Собственный значок метки'
+//                }
+//            }
+//        ];
+////
+//        $scope.map = {
+//            center: [37.64,55.76], zoom: 8
+//        };
 
-        $scope.markers = [
-            {coordinates:[56.56, 38.63], properties: {balloonContent: 'Здесь рыбы нет!'}},
-            {coordinates:[55.16, 39.89], properties: {balloonContent: 'Здесь рыбы тоже нет'}},
-            {coordinates:[55.08, 38.96], properties: {balloonContent: 'А здесь есть!'}}
-        ];
+        $http({method: 'GET', url: '/markers/near/radius/123/43/100'}).
+            success(function (data, status, headers, config) {
+                console.log(data.length);
+                if(data && data.length > 0){
+                    $scope.geoObjects = [];
+                    angular.forEach(data, function(index, marker){
+                        $scope.geoObjects.push(
+                            {
+                                geometry: {
+                                    type: 'Point',
+                                    coordinates: [marker.latitude, marker.longitude]
+                                },
+                                properties: {
+                                    iconContent: 'Метка',
+                                    balloonContent: marker.description
+                                }
+                            }
+                        );
+                    });
 
-        $scope.map = {
-            center: [55.55, 39.84], zoom: 12
-        };
 
-        $scope.geoObjects =
-        {
-            geometry: {
-                type: 'Circle',
-                coordinates: [37.60, 55.76],
-                radius: 10000
-            },
-            properties: {
-                balloonContent: "Радиус круга - 10 км",
-                hintContent: "Подвинь меня"
-            }
-        };
+                    $scope.map = {
+                        center: [43, 123], zoom: 12
+                    };
+                    console.log('All loaded');
+                }
+            }).
+            error(function (data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+
     });
