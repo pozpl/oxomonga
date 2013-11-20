30a93178de89bf2f721d5387c22a7dd3c3fae9e4
Pavel Pozdniak <pozdpl@gmail.com>
2013-11-21 08:04:18 +1100

    master Center on markers average altitude latitude.

--- resources/static/js/app/scripts/controllers/main.js
+++ resources/static/js/app/scripts/controllers/main.js
@@ -2,50 +2,23 @@
 
 angular.module('GeoHashingApp')
     .controller('MainCtrl', function ($scope, $http) {
-//        $scope.geoObjects=[
-//            {
-//                geometry: {
-//                    type: 'Point',
-//                    coordinates: [37.8,55.8]
-//                },
-//                properties: {
-//                    iconContent: 'Метка',
-//                    balloonContent: 'Меня можно перемещать'
-//                }
-//            },
-//            {
-//                geometry: {
-//                    type: 'Point',
-//                    coordinates: [37.6,55.8]
-//                },
-//                properties: {
-//                    iconContent: '1',
-//                    balloonContent: 'Балун',
-//                    hintContent: 'Стандартный значок метки'
-//                }
-//            },
-//            {
-//                geometry: {
-//                    type: 'Point',
-//                    coordinates: [37.56,55.76]
-//                },
-//                properties: {
-//                    hintContent: 'Собственный значок метки'
-//                }
-//            }
-//        ];
-////
-//        $scope.map = {
-//            center: [37.64,55.76], zoom: 8
-//        };
+        $scope.map = {
+            center: [37.64,55.76],
+            zoom: 12
+        };
+
 
         $http({method: 'GET', url: '/markers/near/radius/123/43/100'}).
             success(function (data, status, headers, config) {
                 console.log(data.length);
                 if(data && data.length > 0){
                     $scope.geoObjects = [];
+                    var averageAltitude = 0;
+                    var averageLongitude = 0;
                     angular.forEach(data, function(index, marker){
-                        $scope.geoObjects.push(
+                        averageAltitude = marker.latitude;
+                        averageLongitude = marker.longitude;
+                        $scope.geoObjects .push(
                             {
                                 geometry: {
                                     type: 'Point',
@@ -59,9 +32,12 @@ angular.module('GeoHashingApp')
                         );
                     });
 
+                    averageAltitude = averageAltitude / data.length;
+                    averageLongitude = averageLongitude / data.length;
+
 
                     $scope.map = {
-                        center: [43, 123], zoom: 12
+                        center: [averageAltitude, averageLongitude]
                     };
                     console.log('All loaded');
                 }
