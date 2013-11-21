8b6f8ad2f198a995d9fee4c8faed4128b937527e
Pavel Pozdniak <pozdpl@gmail.com>
2013-11-22 10:01:57 +1100

    master Reload markers on map bounds change

--- resources/static/js/app/scripts/controllers/map.js
+++ resources/static/js/app/scripts/controllers/map.js
@@ -2,23 +2,63 @@
 
 angular.module('GeoHashingApp')
     .controller('MapController', function ($scope, $http) {
+        var _map;
+
+        $scope.afterInit=function(map){
+            _map = map;
+            var centerCoordinates = map.getCenter();
+            fetchMarkersRadius(centerCoordinates[0], centerCoordinates[1], 100);
+        };
+
+        $scope.reloadMarkersOnBoundsChange = function(event){
+            var newBounds = event.get('newBounds');
+            $http({method: 'GET', url: '/markers/rectangle/' + newBounds[0][1] +'/' + newBounds[0][0] +'/' + newBounds[1][1] +'/' + newBounds[1][1]  }).
+                success(function (data, status, headers, config) {
+                    processMarkers(data);
+                }).
+                error(function (data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                });
+        };
+
         $scope.map = {
             center: [37.64,55.76],
             zoom: 12
         };
 
+        var fetchMarkersRadius = function (latitude, longitude, radius) {
+            $http({method: 'GET', url: '/markers/near/radius/' + longitude + '/' + latitude + '/' + radius}).
+                success(function (data, status, headers, config) {
+                    processMarkers(data);
+                }).
+                error(function (data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                });
+            };
 
-        $http({method: 'GET', url: '/markers/near/radius/123/43/100'}).
-            success(function (data, status, headers, config) {
+            var fetchMarkersReactangle = function(bl_lat, bl_lon, ur_lat, ur_lon){
+                $http({method: 'GET', url: '/markers/near/radius/123/43/100'}).
+                    success(function (data, status, headers, config) {
+                        processMarkers(data);
+                    }).
+                    error(function (data, status, headers, config) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                    });
+            };
+
+            var processMarkers = function(data){
                 console.log(data.length);
-                if(data && data.length > 0){
+                if (data && data.length > 0) {
                     $scope.geoObjects = [];
                     var averageAltitude = 0;
                     var averageLongitude = 0;
-                    angular.forEach(data, function(index, marker){
+                    angular.forEach(data, function (index, marker) {
                         averageAltitude = marker.latitude;
                         averageLongitude = marker.longitude;
-                        $scope.geoObjects .push(
+                        $scope.geoObjects.push(
                             {
                                 geometry: {
                                     type: 'Point',
@@ -41,10 +81,6 @@ angular.module('GeoHashingApp')
                     };
                     console.log('All loaded');
                 }
-            }).
-            error(function (data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-            });
-
-    });
+            };
+    }
+);
