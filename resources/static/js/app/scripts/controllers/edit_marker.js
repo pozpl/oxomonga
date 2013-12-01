527a5a149b36b5605c2c2fe0d60dc0f93d04aaf1
Pavel Pozdniak <pozdpl@gmail.com>
2013-12-02 10:00:22 +1100

    master Added texAngular for marker editing purposes

--- resources/static/js/app/scripts/controllers/edit_marker.js
+++ resources/static/js/app/scripts/controllers/edit_marker.js
@@ -1,8 +1,19 @@
 'use strict';
 
 angular.module('GeoHashingApp')
-    .controller('EditMarkerCtrl', function ($scope, $route, $http) {
+    .controller('EditMarkerCtrl', function ($scope, $rootScope, $route, $http) {
 
+        $rootScope.textAngularOpts = {
+            toolbar: [['h1', 'h2', 'h3', 'p', 'pre', 'bold', 'italics', 'ul', 'ol', 'redo', 'undo', 'clear'],['html', 'insertImage', 'insertLink']],
+            classes: {
+                toolbar: "btn-toolbar",
+                toolbarGroup: "btn-group",
+                toolbarButton: "btn btn-default",
+                toolbarButtonActive: "active",
+                textEditor: 'form-control',
+                htmlEditor: 'form-control'
+            }
+        }
 
     });
 
