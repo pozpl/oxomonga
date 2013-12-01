bb80cc034f6a7a58cdef75634f6a7b57651cf390
Pavel Pozdniak <pozdpl@gmail.com>
2013-12-02 08:19:22 +1100

    master Edit marker controller and first approach to view template

--- resources/static/js/app/scripts/app.js
+++ resources/static/js/app/scripts/app.js
@@ -1,12 +1,16 @@
 'use strict';
 
-angular.module('GeoHashingApp', ['ngRoute', 'yaMap'])
+angular.module('GeoHashingApp', ['ngRoute', 'yaMap', 'textAngular'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MarkersListCtrl'
       })
+        .when('/edit/marker/{id}',{
+            templateUrl: 'views/edit_marker.html',
+            controller: 'EditMarkerCtrl'
+        })
       .otherwise({
         redirectTo: '/'
       });
