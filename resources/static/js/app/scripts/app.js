ae0ec41216bae3be95a58ccec54676550c714371
Pavel Pozdniak <pozdpl@gmail.com>
2013-12-09 21:19:16 +1100

    master Route for a single marker view

--- resources/static/js/app/scripts/app.js
+++ resources/static/js/app/scripts/app.js
@@ -1,17 +1,19 @@
 'use strict';
 
 angular.module('GeoHashingApp', ['ngRoute', 'yaMap', 'textAngular'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MarkersListCtrl'
-      })
-        .when('/edit/marker/:id?',{
-            templateUrl: 'views/edit_marker.html',
-            controller: 'EditMarkerCtrl'
-        })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MarkersListCtrl'
+            })
+            .when('/edit/marker/:id?', {
+                templateUrl: 'views/edit_marker.html',
+                controller: 'EditMarkerCtrl'
+            })
+            .when('/show/:id')
+            .otherwise({
+                redirectTo: 'views/single_marker_view.html',
+                controller: 'SingleMarkerViewCtrl'
+            });
+    });
