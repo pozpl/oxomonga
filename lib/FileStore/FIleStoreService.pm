08109da5736da9305e91ae31cd44729e390d935a
Pavel Pozdniak <pozdpl@gmail.com>
2013-12-13 09:05:01 +1100

    master Functions to store file and  get urls for file ids

--- lib/FileStore/FIleStoreService.pm
+++ lib/FileStore/FIleStoreService.pm
@@ -22,8 +22,34 @@ sub store_file(){
     my($self, $path) = @_;
 
     my $id = $self->store_provider->store_path($path);
-
+    return $id;
 }
 
+sub get_file_path(){
+    my($self, $id) = @_;
+
+    return $self->store_provider->fetch_file($id);
+}
+
+sub get_url_for_id(){
+    my($self, $id) = @_;
+
+    my $file_path = $self->store_provider->fetch_file($id);
+    my $url = qr/$file_path/$self->root_path/$self->url_base;
+
+    return $url;
+}
+
+
+sub get_urls_for_ids(){
+    my ($self, $ids_aref) = @_;
+
+    my @urls = [];
+    foreach my $id (@{$ids_aref}){
+        push @urls, $self->get_url_for_id($id);
+    }
+
+    return @urls;
+}
 
 1;
