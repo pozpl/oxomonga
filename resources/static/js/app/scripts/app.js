30ed9c48df96f4c31adbb1a537f2d0e69f662e39
Pavel Pozdniak <pozdpl@gmail.com>
2013-12-16 08:12:55 +1100

    master Intercepting 401 response from the server

--- resources/static/js/app/scripts/app.js
+++ resources/static/js/app/scripts/app.js
@@ -42,6 +42,33 @@ geoHashingApp.config(function($stateProvider, $urlRouterProvider) {
         });
 });
 
+geoHashingApp.config(function ($httpProvider) {
+
+    var logsOutUserOn401 = ['$q', '$location', function ($q, $location) {
+        var success = function (response) {
+            return response;
+        };
+
+        var error = function (response) {
+            if (response.status === 401) {
+                //redirect them back to login page
+                $location.path('/login');
+
+                return $q.reject(response);
+            }
+            else {
+                return $q.reject(response);
+            }
+        };
+
+        return function (promise) {
+            return promise.then(success, error);
+        };
+    }];
+
+    $httpProvider.responseInterceptors.push(logsOutUserOn401);
+});
+
 geoHashingApp.run(function($rootScope, $location, AuthenticationService) {
 
    $rootScope.$on('$stateChangeStart', function (ev, to, toParams, from, fromParams) {
