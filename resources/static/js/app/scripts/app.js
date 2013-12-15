676add1b4ad1d703eaff652f2e088e40a247a6bc
Pavel Pozdniak <pozdpl@gmail.com>
2013-12-16 08:09:39 +1100

    master Intercept change state event and check a state of an application

--- resources/static/js/app/scripts/app.js
+++ resources/static/js/app/scripts/app.js
@@ -44,20 +44,9 @@ geoHashingApp.config(function($stateProvider, $urlRouterProvider) {
 
 geoHashingApp.run(function($rootScope, $location, AuthenticationService) {
 
-    // enumerate routes that don't need authentication
-    var routesThatDontRequireAuth = ['/login'];
-
-    // check if current location matches route
-    var routeClean = function (route) {
-        return _.find(routesThatDontRequireAuth,
-            function (noAuthRoute) {
-                return _.str.startsWith(route, noAuthRoute);
-            });
-    };
-
-    $rootScope.$on('$stateChangeStart', function (ev, to, toParams, from, fromParams) {
+   $rootScope.$on('$stateChangeStart', function (ev, to, toParams, from, fromParams) {
         // if route requires auth and user is not logged in
-        if (!routeClean($location.url()) && !AuthenticationService.isLoggedIn()) {
+        if (to.data.needAuth && !AuthenticationService.isLoggedIn()) {
             // redirect back to login
             $location.path('/login');
         }
