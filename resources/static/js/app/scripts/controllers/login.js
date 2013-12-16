8fc3285420df2398171890469ca2a129895dde88
Pavel Pozdniak <pozdpl@gmail.com>
2013-12-17 08:24:10 +1100

    master Go to the page intended before an authentication

--- resources/static/js/app/scripts/controllers/login.js
+++ resources/static/js/app/scripts/controllers/login.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('GeoHashingApp')
-    .controller('LoginCtrl', ['$scope', '$rootScope', '$http', 'AuthenticationService'
-        , function ($scope, $rootScope, $http, AuthenticationService) {
+    .controller('LoginCtrl', ['$scope', '$rootScope', '$http', '$state' ,'AuthenticationService'
+        , function ($scope, $rootScope, $http, $state, AuthenticationService) {
 
+            $scope.userName = '';
+            $scope.password = '';
 
+            AuthenticationService.setLoggedIn(true);
+            if(AuthenticationService.getLastUnauthenticatedState()){
+                $state.go(AuthenticationService.getLastUnauthenticatedState());
+            }
 
         }
     ]);
