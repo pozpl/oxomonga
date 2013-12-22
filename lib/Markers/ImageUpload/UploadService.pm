31b43acb7369285824bad3adefd3664d68563456
Pavel Pozdniak <pozdpl@gmail.com>
2013-12-23 08:31:39 +1100

    master Broadened upload service responsibility to add uploaded images ids to marker

--- lib/Markers/ImageUpload/UploadService.pm
+++ lib/Markers/ImageUpload/UploadService.pm
@@ -6,27 +6,40 @@ has 'file_store' => (
     'isa' => 'FileStore::FileStoreService',
 );
 
+has 'markers_repository' => (
+    'is' => 'ro',
+    'isa' => 'Markers::MarkerRepository',
+);
+
 #########################################################################
 # Usage      : @files_ids = save_uploads($uploads_aref);
-# Purpose    : save uplads to filestorage and get ids of files
-# Returns    : array of file ids
-# Parameters : $uplads_aref array reference for Wev::Request::Upload
+# Purpose    : save uploads to file storage and get ids of files
+# Returns    : none
+# Parameters : $marker_id - id of a marker to add images to
+#               $uplads_aref array reference for Wev::Request::Upload
 # Throws     : no exceptions
 # Comments   : ???
 # See Also   : n/a
 sub save_upload(){
-    my ($self, $uploads_aref) = @_;
+    my ($self, $marker_id, $uploads_aref) = @_;
+
+    my @uploaded_files_ids = _save_to_store($uploads_aref);
+    $self->markers_repository->add_images_to_marker($marker_id, \@uploaded_files_ids);
+}
+
+sub _save_to_store(){
+    my ($self, $uploads_aref) =@_;
 
     my @uploaded_files_ids = ();
-    foreach my $tmp_file (@{$uploads_aref}){
-        my $tmp_file_name = $tmp_file->tempname;
-        if(-e $tmp_file_name){ #here will be proper validation
-            my $file_id = $self->file_store->store_file($tmp_file_name);
-            push @uploaded_files_ids, $file_id;
+        foreach my $tmp_file (@{$uploads_aref}){
+            my $tmp_file_name = $tmp_file->tempname;
+            if(-e $tmp_file_name){ #here will be proper validation
+                my $file_id = $self->file_store->store_file($tmp_file_name);
+                push @uploaded_files_ids, $file_id;
+            }
         }
-    }
 
-    return @uploaded_files_ids;
+        return @uploaded_files_ids;
 }
 
 1;
