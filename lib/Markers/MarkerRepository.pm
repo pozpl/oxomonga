9a5589fdfff1ea79d8124213c536c57c082ad21e
Pavel Pozdniak <pozdpl@gmail.com>
2013-12-22 23:02:41 +1100

    master Delete image from marker

--- lib/Markers/MarkerRepository.pm
+++ lib/Markers/MarkerRepository.pm
@@ -111,12 +111,20 @@ sub list_markers(){
 sub add_image_to_marker(){
     my ($self, $marker_id, $image_id) = @_;
 
-     my $find_query = Tie::IxHash->new({'_id' => MongoDB::OID->new('value' => $id)});
+     my $find_query = Tie::IxHash->new({'_id' => MongoDB::OID->new('value' => $marker_id)});
      my $update_query = {
-                            $addToSet: { 'images': { $image_id } }
+                            '$addToSet': { 'images': { $image_id } }
                         };
 
-     $self->markers_collection->update($query, );
+     $self->markers_collection->update($query, $update_query);
+}
+
+sub delete_image_from_marker(){
+    my ($self, $marker_id, $image_id) = @_;
+
+    my $find_query = Tie::IxHash->new({'_id' => MongoDB::OID->new('value' => $marker_id)});
+    my $delete_query = {'$pull': 'images' : $image_id};
+    $self->markers_collection->update($query, $delete_query);
 }
 
 sub delete_by_id(){
