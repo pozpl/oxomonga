4fd0d5027534d111796cb71aeb01bb003db5b63b
Pavel Pozdniak <pozdpl@gmail.com>
2013-12-23 12:36:44 +1100

    master Simple upload controller

--- lib/Markers/ImageUpload/UploadController.pm
+++ lib/Markers/ImageUpload/UploadController.pm
@@ -1,4 +1,24 @@
 package Markers::ImageUpload::UploadController;
 use Moose;
+use JSON;
+
+has 'upload_service' => (
+    'is' => 'ro',
+    'isa' => 'Markers::ImageUpload::UploadService',
+);
+
+
+
+sub process_upload(){
+    my ($self, $request) = @_;
+
+    my $uploads_href = $request->all_uploads();
+    my @images_urls = ();
+    if(exits($uploads_href->{'up_image'})){
+        @images_urls = $this->upload_service->save( $uploads_href->{'up_image'} );
+    }
+
+    return JSON->new()->encode(\@images_urls);
+}
 
 1;
