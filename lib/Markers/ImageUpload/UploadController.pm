d7201b7bc6cf6720bce3c789a4988e26219daf4b
Pavel Pozdniak <pozdpl@gmail.com>
2013-12-30 10:02:16 +1100

    master Existis function name fix

--- lib/Markers/ImageUpload/UploadController.pm
+++ lib/Markers/ImageUpload/UploadController.pm
@@ -1,6 +1,7 @@
 package Markers::ImageUpload::UploadController;
 use Moose;
 use JSON;
+use Data::Dump qw(dump);
 
 has 'upload_service' => (
     'is' => 'ro',
@@ -14,10 +15,11 @@ sub process_upload(){
     my ($self, $request) = @_;
 
     my $uploads_href = $request->all_uploads();
+    print dump($uploads_href);
     my $marker_id = $request->param('marker_id');
     my @images_urls = ();
-    if(exits($uploads_href->{'file'})){
-        @images_urls = $self->upload_service->save( $uploads_href->{'file'} );
+    if(exists($uploads_href->{'file'})){
+        @images_urls = $self->upload_service->save_upload($marker_id, $uploads_href->{'file'} );
     }
 
     return JSON->new()->encode(\@images_urls);
