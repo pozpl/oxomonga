bdc0a0156ae21bd548b801af8af6f8046b1c1f97
Pavel Pozdnyak <pozdpl@gmail.com>
2014-01-27 08:15:28 +1100

    master If we have authenticated user with user_id in session, then build user token and store it in session.

--- lib/Auth/Middleware/Token.pm
+++ lib/Auth/Middleware/Token.pm
@@ -6,7 +6,7 @@ extends 'Plack::Middleware';
 
 use HTTP::Throwable::Factory 'http_exception';
 use OX::Request;
-
+use Auth::UserToken;
 #has model => (
 #  is => 'ro',
 #  isa => 'OXauth::Schema' ,
@@ -19,21 +19,24 @@ sub call {
 
   my $req = OX::Request->new(env => $env);
 
-  my $login_url = $req->uri_for('login');
+  #my $login_url = $req->uri_for('login');
 
   # load the user data if there's a user_id set in the session
   if ( my $id = $req->session->{'user_id'} ) {
-    $req->session->{user} = $self->load_user( $id );
+    my $userToken = Auth::UserToken->new(
+        'user_id' => $id;
+    );
+    $req->session->{'user_token'} = $user_token;
   }
 
   # if we have a user or if we're trying to login, carry on
-  if ( $req->session->{user} || $req->uri->path eq $login_url) {
-    return $self->app->($env);
-  }
-  # otherwise redirect to the login url
-  else {
-    return $req->new_response->redirect( $login_url );
-  }
+#  if ( $req->session->{user} || $req->uri->path eq $login_url) {
+#    return $self->app->($env);
+#  }
+#  # otherwise redirect to the login url
+#  else {
+#    return $req->new_response->redirect( $login_url );
+#  }
 }
 
 __PACKAGE__->meta->make_immutable;
