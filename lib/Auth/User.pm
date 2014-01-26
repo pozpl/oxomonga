4a2ec2f483a898a57cf36864bf29084025e54f32
Pavel Pozdnyak <pozdpl@gmail.com>
2014-01-27 08:26:39 +1100

    master Preliminary structure for user dataholder

--- lib/Auth/User.pm
+++ lib/Auth/User.pm
@@ -1,3 +1,31 @@
 package Auth::User;
 
+use Moose;
+
+
+has 'id' => (
+    'is' => 'ro',
+    'isa' => 'Int',
+);
+
+
+has 'login' => (
+    'is' => 'ro',
+    'isa' => 'Str',
+);
+
+has 'friendly_name' => (
+    'is' => 'ro',
+    'isa' => 'Str',
+);
+
+has 'provider' => (
+    'is' => 'ro',
+    'isa' => 'Str'
+);
+
+has 'avatar' => (
+    'is' => 'ro',
+    'isa' => 'Str'
+);
 1;
