5a7f232d6db30a6c8f80360fb7a3a1b846b131da
Pavel Pozdnyak <pozdpl@gmail.com>
2014-02-04 08:18:25 +1100

    master During registration just send user data into a server

--- resources/static/js/app/scripts/controllers/register_user.js
+++ resources/static/js/app/scripts/controllers/register_user.js
@@ -2,8 +2,28 @@
 
 angular.module('GeoHashingApp')
     .controller('RegisterUserCtrl', ['$scope', '$rootScope', '$stateParams', '$http', '$upload',
-        function ($scope, $rootScope, $stateParams, $http, $upload) {
+        function ($scope, $http, $state, AuthenticationService) {
 
+            $scope.submit = function () {
+                if ($scope.password == $scope.reenter_password) {
+                    var userRegistrationData = {
+                        'login': $scope.login,
+                        'email': $scope.email,
+                        'password': $scope.password
+                    };
+                    $http({method: 'POST', url: '/user/register', data: userRegistrationData }).
+                        success(function (data, status, headers, config) {
+                            if (data && data.id) {
+                                AuthenticationService.setLoggedIn(true);
+                                $state.go('main_state');
+                            }
+                        }).
+                        error(function (data, status, headers, config) {
+                            // called asynchronously if an error occurs
+                            // or server returns response with an error status.
+                        });
+                }
+            };
 
         }
     ]);
