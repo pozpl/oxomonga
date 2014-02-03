f432b89cd1603ec4c5df24e84779ae06a68d2dbf
Pavel Pozdnyak <pozdpl@gmail.com>
2014-02-04 07:58:31 +1100

    master Simple controller for user registration

---
+++ resources/static/js/app/scripts/controllers/register_user.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('GeoHashingApp')
+    .controller('RegisterUserCtrl', ['$scope', '$rootScope', '$stateParams', '$http', '$upload',
+        function ($scope, $rootScope, $stateParams, $http, $upload) {
+
+
+        }
+    ]);
+
+
