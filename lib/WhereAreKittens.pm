907ead1ea2bd8d4862e1d675525ff225fe74c8a8
Pavel Pozdnyak <pozdpl@gmail.com>
2014-02-11 16:18:43 +1100

    master Moved wrapper into the main application

--- lib/WhereAreKittens.pm
+++ lib/WhereAreKittens.pm
@@ -4,9 +4,9 @@ use OX;
 with('DI::DIRole');
 with('Routes::RoutesRole');
 
-routes as {
+router as {
      wrap 'Plack::Middleware::Session' => ( store => literal( 'File' ));
-     wrap 'Auth::Middleware::Token' => ( user_repository => 'user_repository' );
-}
+     wrap 'Auth::Middleware::Token' => ( user_repository => 'users_repository' );
+};
 
 1;
