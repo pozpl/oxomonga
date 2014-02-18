3733651fd802da4fc8f2417a663f8083df163daa
Pavel Pozdnyak <pozdpl@gmail.com>
2014-02-19 10:06:27 +1100

    issue-2 Controller with oauth2

--- lib/Auth/Login/OAuthLoginController.pm
+++ lib/Auth/Login/OAuthLoginController.pm
@@ -3,7 +3,8 @@ package Auth::Login::OAuthLoginController;
 use Auth::UsersRepository;
 use Auth::User;
 use JSON;
-use OAuth::Simple;
+use LWP::Authen::OAuth2;
+use Data::Dump qw(dump);
 
 has 'user_repository' => (
     'is'  => 'ro',
@@ -21,20 +22,29 @@ sub login {
 
    my $provider_credentials = $self->oauth_conf->{$provider};
 
-      my $return = 0;
+   my $return = 0;
+   $code = $req->param('code');
 
-      if($provider_credentials){
-        my $oauth = OAuth::Simple->new(
-                      app_id     => $provider_credentials->{'app_id'},
-                      secret     => $provider_credentials->{'app_secret'},
-                      postback   => $request->uri_for(
-                                            'name' => 'oauth_login',
-                                            'provider' => $provider
-                                            ),
-                  );
-        my $access = $oauth->request_access_token( {url => $provider_credentials->{'access_token_url'}, code => $args->{code}, raw => 1} );
-        my $profile_data = $oauth->request_data( {url => $provider_credentials->{'profile_data_url'}, access_token => $access} );
-    }
+   if($provider_credentials && $code){
+       my $oauth2 = LWP::Authen::OAuth2->new(
+                             client_id => $provider_credentials->{'app_id'},
+                             client_secret => $provider_credentials->{'app_secret'},
+                             service_provider => $provider,
+                             redirect_uri => $request->uri_for(
+                                                'name' => 'oauth_login',
+                                                'provider' => $provider
+                                              ),
+
+                             # Optional hook, but recommended.
+                             #save_tokens => \&save_tokens,
+                             # This is for when you have tokens from last time.
+                             #token_string => $token_string.
+                         );
+
+        # URL for user to go to to start the process.
+       $token_string  = $oauth2->request_tokens(code => $code);
+
+  }
   return JSON->new->encode($authentication_status);
 }
 
@@ -46,16 +56,24 @@ sub check_credentials_oauth(){
     my $return = 0;
 
     if($provider_credentials){
-         my $oauth = OAuth::Simple->new(
-              app_id     => $provider_credentials->{'app_id'},
-              secret     => $provider_credentials->{'app_secret'},
-              postback   => $request->uri_for(
-                                    'name' => 'oauth_login',
-                                    'provider' => $provider
-                                    ),
-          );
-         my $url = $oauth->authorize( {url => $provider_credentials->{'oauth_dialog'}, scope => 'email', response_type => 'code'} );
-         # Your web app redirect method.
+         my $oauth2 = LWP::Authen::OAuth2->new(
+                                      client_id => $provider_credentials->{'app_id'},
+                                      client_secret => $provider_credentials->{'app_secret'},
+                                      service_provider => $provider,
+                                      redirect_uri => $request->uri_for(
+                                                         'name' => 'oauth_login',
+                                                         'provider' => $provider
+                                                       ),
+
+                                      # Optional hook, but recommended.
+                                      #save_tokens => \&save_tokens,
+                                      # This is for when you have tokens from last time.
+                                      #token_string => $token_string.
+                                  );
+
+                     # URL for user to go to to start the process.
+         my $url = $oauth2->authorization_url();
+
          $return = $request->new_response->redirect($url);
 
     }else{
