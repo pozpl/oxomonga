b075f1b1fe00227c520dc02f1b917411752bbb3e
Pavel Pozdnyak <pozdpl@gmail.com>
2014-02-27 08:00:39 +1100

    issue-3 Bind longtude and latitude into the directive

--- resources/static/js/app/scripts/directives/place_marker_directive.js
+++ resources/static/js/app/scripts/directives/place_marker_directive.js
@@ -4,6 +4,10 @@ angular.module('GeoHashingApp')
     }])
     .directive('placeMarker', function() {
         return {
+            scope: {
+                'longitude': '=',
+                'latitude' : '='
+            },
             templateUrl: 'views/place_marker_directive.html'
         };
     });
