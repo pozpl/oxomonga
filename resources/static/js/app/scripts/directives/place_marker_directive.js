4d428b304075a386f2a0ec08836aea1a8651e650
Pavel Pozdnyak <pozdpl@gmail.com>
2014-02-27 10:00:41 +1100

    issue-3 If longitude and latitude are not defined from outside get it from maps autodetection

--- resources/static/js/app/scripts/directives/place_marker_directive.js
+++ resources/static/js/app/scripts/directives/place_marker_directive.js
@@ -1,13 +1,41 @@
 angular.module('GeoHashingApp')
-    .controller('PlaceMarkerController',['$scope' , function($scope) {
+    .controller('PlaceMarkerController',['$scope', function($scope) {
 
     }])
-    .directive('placeMarker', function() {
+    .directive('placeMarker',[function() {
+
         return {
             scope: {
                 'longitude': '=',
                 'latitude' : '='
             },
-            templateUrl: 'views/place_marker_directive.html'
+            templateUrl: 'views/place_marker_directive.html',
+            controller: function($scope){
+
+                $scope.beforeInit = function(){
+
+                    var coords = [];
+                    if($scope.longitude && $scope.longitude){
+                        coords = [$scope.longitude, $scope.longitude]
+                    }else{
+                        var geolocation = ymaps.geolocation;
+                        coords = [geolocation.longitude, geolocation.latitude];
+                    }
+                    $scope.center = coords;
+
+                    $scope.makerObject =  {
+                        geometry: {
+                            type: 'Point',
+                            coordinates: coords //[$scope.longitude, $scope.latitude]
+                        },
+                        properties: {
+                            iconContent: 'Метка',
+                            balloonContent: 'Меня можно перемещать',
+                            draggable: true
+                        }
+                    }
+                }
+            }
+
         };
-    });
+    }]);
