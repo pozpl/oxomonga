7ad2f87b343a8aca2acdf65e071149e015816679
Pavel Pozdnyak <pozdpl@gmail.com>
2014-03-04 23:09:52 +1100

    issue-3 Get coordinates of dragged marker and set input parameters according to it

--- resources/static/js/app/scripts/directives/place_marker_directive.js
+++ resources/static/js/app/scripts/directives/place_marker_directive.js
@@ -11,6 +11,16 @@ angular.module('GeoHashingApp')
             },
             templateUrl: 'views/place_marker_directive.html',
             controller: function($scope){
+                $scope.makerObject =  {
+                    geometry: {
+                        type: 'Point'
+//                        coordinates: coords
+                    },
+                    properties: {
+                        iconContent: 'Marker',
+                        balloonContent: 'You can move me'
+                    }
+                }
 
                 $scope.beforeInit = function(){
 
@@ -22,17 +32,15 @@ angular.module('GeoHashingApp')
                     var coords = [$scope.longitude, $scope.latitude];
                     $scope.center = coords;
 
-                    $scope.makerObject =  {
-                        geometry: {
-                            type: 'Point',
-                            coordinates: coords
-                        },
-                        properties: {
-                            iconContent: 'Marker',
-                            balloonContent: 'You can move me'
-                        }
-                    }
-                }
+                    $scope.makerObject.geometry.coordinates = coords;
+                };
+
+                $scope.dragEnd = function($event){
+                    var coords = $event.get('target').geometry.getCoordinates();
+                    $scope.longitude = coords[0];
+                    $scope.latitude  = coords[1];
+                };
+
             }
 
         };
