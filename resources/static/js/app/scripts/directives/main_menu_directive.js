a27e73f8d9a035c3f30fd66676a5a11bbaab10b4
Pavel Pozdnyak <pozdpl@gmail.com>
2014-03-07 08:06:04 +1100

    master Simple menu highlighting

--- resources/static/js/app/scripts/directives/main_menu_directive.js
+++ resources/static/js/app/scripts/directives/main_menu_directive.js
@@ -1,8 +1,12 @@
 angular.module('GeoHashingApp')
-    .controller('MainMenuController',['$scope' , 'AuthenticationService', function($scope, AuthenticationService) {
+    .controller('MainMenuController',['$scope', '$location', 'AuthenticationService', function($scope, $location, AuthenticationService) {
         $scope.user = {
             isLoggedIn: AuthenticationService.isLoggedIn(),
             userId: AuthenticationService.getUserId()
+        };
+
+        $scope.isActive = function (viewLocation) {
+            return viewLocation === $location.path();
         };
     }])
     .directive('mainMenu', function() {
